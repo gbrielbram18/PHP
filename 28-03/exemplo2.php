9d1afee105fbb68d7fa642a571997b519a364816
Gabriel <biel30081@gmail.com>
2025-03-28 15:53:58 -0300

    adicionando arquivo

---
+++ 28-03/exemplo2.php
@@ -0,0 +1,17 @@
+<?php 
+echo" exemplo 2";
+
+$num = 21;
+ 
+if ($num < 20) {
+echo"O numero e menor que 20";
+} else if($num == 20) {    
+    echo "Este numero e igual a 20";
+} else if($num >= 21) {
+    echo "o numero e menor que 20";
+} else{
+    echo " O numero e menor que 20";
+}
+
+
+?>
---
+++ 28-03/exer1.php
@@ -0,0 +1,7 @@
+<?php  
+## faça uma condição, se o numero e maior que 2
+$numero1 = 50;
+$numero2 = 10;
+
+
+?>
