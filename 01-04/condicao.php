267560d7468bbc794b5dbe56e55a736fa78def2f
Gabriel <biel30081@gmail.com>
2025-04-01 16:15:04 -0300

    adicionando exercio e alterações

--- 01-04/condicao.php
+++ 01-04/condicao.php
@@ -5,5 +5,26 @@
     //aqui pode usar || ou tambem pode usar or em php
      if ($idade=== 18 or $idade >18) {
         echo"$nome, voce tem $idade ou mais anos de idade!";
-     } 
+    }
+
+
+
+    echo"<br><br>";
+    
+    //aqui pode usar && ou tambem pode usar and em php
+    if ($idade=== 18 and $nome === "Gabriel") {
+        echo "voce tem $idade anos. Pode entrar";
+    }else{
+        echo "nao deu certo";
+    }
+
+    
+
+
+
+
+
+
+
+
 ?>
---
+++ 01-04/exer3.php
@@ -0,0 +1,18 @@
+<?php
+$dia = "sabado ou domingo";
+$semana=" dia util";
+
+// or ||
+
+if($dia === "sabado "|| $dia ==="domingo"){
+echo" e final de semana";
+} else {
+    echo "dia util! hora de programar";
+}
+
+
+
+
+
+
+?>
