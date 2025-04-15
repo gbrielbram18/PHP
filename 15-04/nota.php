02df9c6afe14bcffc66a1ee1cf90abf2df438413
Gabriel <biel30081@gmail.com>
2025-04-15 15:55:00 -0300

    adicionando novas atividades

---
+++ 15-04/nota.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+</head>
+<body>
+    <main class="container col-md-6 mt-5 text-center">
+        <form action="nota-detalhes.php" method="get">
+            <section class="form-control">
+                <label for="nota" class="d-block  mb-2 text-sucess">Nota:</label>
+                <input type="number" class="form-control" id="nota" name="nota" placeholder="Digite a sua nota!">
+            </section>
+
+            <section class="d-block mt-3">
+                <input type="submit" class="btn btn-success" value="verificar">
+            </section>
+        </form>
+    </main>
+    
+</body>
+</html>
---
+++ 15-04/numero.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+</head>
+<body>
+    <div>
+        <form action="">
+            <label for="txtNumero">Digite o número da sorte</label>
+            <input type="text" id="txtNumero" name="txtNumero" value="Tentar sorte">
+        </form>
+    </div>
+    
+</body>
+</html>
